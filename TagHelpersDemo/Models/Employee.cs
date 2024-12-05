d8b649d699a93dabea9764298867eef711aa3729
Abhishek <Abhishekgwari14sep@gmail.com>
2024-12-05 13:03:28 +0530

    Form-Tag-Helper

---
+++ TagHelpersDemo/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace TagHelpersDemo.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Designation {  get; set; }
+
+        public int Salary { get; set; }
+    }
+}
